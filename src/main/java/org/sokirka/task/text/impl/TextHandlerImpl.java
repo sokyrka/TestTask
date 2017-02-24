b491e38f3b98f410594ae3a03c7ff9b9835609d4
eugine_sokyrka <sokyrka_evgeniy@yahoo.com>
2017-02-25 00:09:05 +0200

    Refactoring.

--- src/main/java/org/sokirka/task/text/impl/TextHandlerImpl.java
+++ src/main/java/org/sokirka/task/text/impl/TextHandlerImpl.java
@@ -3,6 +3,7 @@ package org.sokirka.task.text.impl;
 import org.sokirka.task.text.TextHandler;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
@@ -15,7 +16,9 @@ public class TextHandlerImpl implements TextHandler {
 
     @Override
     public Map<String, Integer> getModifiedText(String words) {
-        StringTokenizer tokenizer = new StringTokenizer(words, " .,;-@\r");
+        String[] filteredArray = words.split("[\\p{Punct}\\p{Digit}]");
+        String filteredString = Arrays.toString(filteredArray);
+        StringTokenizer tokenizer = new StringTokenizer(filteredString, " .,;-@«»×·—–✖[]©\r");
         Map<String, Integer> map = new TreeMap<>();
         map.put(" Total count", tokenizer.countTokens());
 
@@ -24,12 +27,10 @@ public class TextHandlerImpl implements TextHandler {
 
         while (tokenizer.hasMoreTokens()) {
             word = tokenizer.nextToken();
-
             count = map.get(word);
             count = (count == null) ? 1 : count + 1;
             map.put(word, count);
         }
-
         return map;
     }
 }
