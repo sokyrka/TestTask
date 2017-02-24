212b9bb98d21c20b63f1eecd31d2cb0a5e749ffb
Eugine Sokirka <sokyrka_evgeniy@yahoo.com>
2017-02-24 18:00:36 +0200

    Has been changed controller.

--- src/main/java/org/sokirka/task/controllers/MainController.java
+++ src/main/java/org/sokirka/task/controllers/MainController.java
@@ -1,7 +1,13 @@
 package org.sokirka.task.controllers;
 
+import org.sokirka.task.exceptions.NotValidURLPageException;
+import org.sokirka.task.expr.ExpressionChecker;
+import org.sokirka.task.parser.Parser;
+import org.sokirka.task.text.TextHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -10,10 +16,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-    @ResponseBody
-    @RequestMapping(path = "/")
-    public String sayHello() {
-        return "Hello Spring World";
+    private ExpressionChecker checker;
+    private Parser parser;
+    private TextHandler textHandler;
+
+    @Autowired
+    public MainController(ExpressionChecker checker, Parser parser, TextHandler textHandler) {
+        this.checker = checker;
+        this.parser = parser;
+        this.textHandler = textHandler;
     }
 
+    @ResponseBody
+    @RequestMapping(value = "/")
+    public String inputRequestParam(@RequestParam(value = "url", required = true) String url) throws NotValidURLPageException {
+        if (!checker.isValid(url))
+            throw new NotValidURLPageException("Page not valid");
+        String parsedText = parser.parse(url);
+        return textHandler.getPerformedText(parsedText).toString();
+    }
 }
